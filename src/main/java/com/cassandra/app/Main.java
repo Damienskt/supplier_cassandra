112f8a373a20d34b694b835ecacd38ad799c5ca4
Chua Wei Wen <weiwen.chua5@gmail.com>
2018-09-25 17:31:23 +0800

    Initialize

---
+++ src/main/java/com/cassandra/app/Main.java
@@ -0,0 +1,11 @@
+package com.cassandra.app;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/cassandra/app/Table.java
@@ -0,0 +1,14 @@
+package com.cassandra.app;
+
+/**
+ * Global constants for table name
+ */
+public class Table {
+    public static String WAREHOUSE = "warehouse";
+    public static String DISTRICT = "district";
+    public static String CUSTOMER = "customer";
+    public static String ORDER = "orders";
+    public static String ITEM = "item";
+    public static String ORDER_LINE = "order_line";
+    public static String STOCK = "stock";
+}
